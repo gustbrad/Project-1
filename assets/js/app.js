cddeeca32d760c06794ad0130a705cbf187806a2
Jesse Harrah <defiledspec@gmail.com>
2018-06-23 14:47:38 -0500

    added html boilerplate, js lib links, and added firebase

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyDi0DWfWqD-EyT4HYOVURLP-5HoD4iInIQ",
+    authDomain: "playlist-30555.firebaseapp.com",
+    databaseURL: "https://playlist-30555.firebaseio.com",
+    projectId: "playlist-30555",
+    storageBucket: "",
+    messagingSenderId: "752707560404"
+  };
+  firebase.initializeApp(config);

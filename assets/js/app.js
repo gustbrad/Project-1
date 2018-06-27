dd60b5057320180569d7538cc789f82137ec1d56
DefiledSpec <defiledspec@gmail.com>
2018-06-26 19:20:31 -0500

    style changes to the results displayed

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,20 +18,25 @@ let usersChannel = { //Object for default user todo: need to populate this when 
 };
 
 function displayResults(results) {
-  searchResults.empty();
-  for (let item of results) {
-    let {channelId, channelTitle, description, publishedAt, thumbnails, title} = item.snippet;
-    //item.id {kind, videoId}, item.kind, item.snippet {^}
-    let resultItemDiv = $('<div>').attr({id: item.id.videoId}).addClass('youtube-search-result')
-      .append($('<img>').attr({src: thumbnails.default.url, width: thumbnails.default.width, height: thumbnails.default.height,style: 'float: left;'}))
-      .append($('<h6>').attr({}).text(title)).addClass('text-truncate') //would like to find out how to truncate this text
-      .append($('<p>').text(description)) //should be truncated as well
-      .append($('<span>').text(channelTitle))
-      .append($('<span>').text(moment(publishedAt).format('lll')).attr({style: 'float: right;'}))//can be formatted better
-      .append($('<div>').addClass('clearfix')) // to clear the floats in the div
-    searchResults.append(resultItemDiv);
-  
+  if(results.length > 0) {
+    searchResults.empty();
+    let resultItemDiv
+    for (let item of results) {
+      let {channelId, channelTitle, description, publishedAt, thumbnails, title} = item.snippet;
+      //item.id {kind, videoId}, item.kind, item.snippet {^}
+      resultItemDiv = $('<div>').attr({id: item.id.videoId,style: 'float: left;'}).addClass('youtube-search-result')
+      let thumbnail = $('<img>').attr({src: thumbnails.default.url, width: thumbnails.default.width, height: thumbnails.default.height})
+      let detailsDiv = $('<div>').attr({}).addClass('youtube-search-result-details')
+        .append($('<h6>').addClass('text-truncate result-title').attr({}).text(title)) //would like to find out how to truncate this text
+        .append($('<p>').addClass('text-truncate result-description').text(description)) //should be truncated as well
+        .append($('<span>').addClass('result-creator').text(channelTitle))
+        .append($('<span>').addClass('result-date-created').text(moment(publishedAt).format('lll')).attr({}))//can be formatted better
+        .append($('<div>').addClass('clearfix')) // to clear the floats in the div
+      resultItemDiv.append(thumbnail).append(detailsDiv)
+      searchResults.append(resultItemDiv);  
+    }
+    searchResults.show();
   }
-}
+};
 
 searchBtn.on('click', searchYt)

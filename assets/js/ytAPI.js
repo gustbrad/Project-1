767a661c84ca1e195fbe00cb5fee49e5a707f7ac
Jesse Harrah <defiledspec@gmail.com>
2018-06-27 23:02:36 -0500

    testing ajax calls to youtube api

--- assets/js/ytAPI.js
+++ assets/js/ytAPI.js
@@ -1,7 +1,8 @@
+const API_KEY = `AIzaSyAowmVgFQkCDODU9HZ9SFVmeQlniBW_zfU`;
+
 function searchYoutube() {
-    fetch(`https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.search.list?part=snippet&order=viewCount&q=skateboarding+dog&type=video&videoDefinition=high`)
-        .then(res => res.json())
-        .then(data => console.log(data))
+    $.ajax({'url': `https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.search.list?part=snippet&order=viewCount&q=skateboarding+dog&type=video&videoDefinition=high&key=${API_KEY}`, 'method': 'GET'})
+        .then(res => console.log(res))
         .catch(err => console.log(err))
 };
 searchYoutube();
